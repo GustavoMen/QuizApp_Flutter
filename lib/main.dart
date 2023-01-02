b9cbc96b8e9311978c68c456f16298421ddec3d6
unknown <contato.gustavomendonca@gmail.com>
2023-01-02 19:09:35 -0300

    Using StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(QuizApp());
-}
+void main() => runApp(QuizApp());
 
-class QuizApp extends StatelessWidget {
+class QuestionAppState extends State<QuizApp> {
+  int selectedQuestion = 0;
+
+  void reply() {
+    setState(() {
+      selectedQuestion++;
+    });
+
+    print('Pergunta Respondida');
+    print(selectedQuestion);
+  }
+
+  final List<String> questions = ['Pergunta 1', 'Pergunta Dois'];
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Olá Flutter!!'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "ola texto",
+            ),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text(questions[selectedQuestion]),
+              ElevatedButton(
+                child: const Text('Resposta 1'),
+                onPressed: reply,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      selectedQuestion == 0 ? Colors.blue : Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+              ),
+              ElevatedButton(
+                child: const Text('Resposta 2'),
+                onPressed: reply,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  foregroundColor: Colors.white,
+                ),
+              )
+            ],
+          )),
     );
+  }
+}
+
+class QuizApp extends StatefulWidget {
+  QuestionAppState createState() {
+    return QuestionAppState();
   }
 }
